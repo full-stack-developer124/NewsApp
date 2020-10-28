0645f520f886d372a5ecca96256fce455acb34b7
Happiness <happiness@Happinesss-Mac-mini.local>
2020-10-28 17:01:01 +0100

    newsapp v1

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import TabScreen from './src/screens/TabScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <TabScreen />
+  );
+};
+
+export default App;
